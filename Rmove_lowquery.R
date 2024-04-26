2efe43a44a33ab7b976b6d3b059706165170c9a4
smartise <olivier1996collard@hotmail.fr>
2024-04-26 12:06:40 +0200

    changing packages

--- Rmove_lowquery.R
+++ Rmove_lowquery.R
@@ -1,9 +1,16 @@
-library(data.table)
-library(ggplot2)
-library(vegan)
-library(RColorBrewer)
-library(stringr)
-library(ade4)
+
+
+# List of required packages
+required_packages <- c("data.table", "ggplot2", "vegan", "RColorBrewer", "stringr", "ade4")
+
+for (pkg in required_packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+    library(pkg, character.only = TRUE)
+  } else {
+    library(pkg, character.only = TRUE)
+  }
+}
 
 blast=read.csv("Blast_res.txt", header = F, sep = "\t", dec = ".")
 colnames(blast)=c("ASV", "qlen", "GIAccNum", "slen", "Ali_length", "Gaps", "Qcovs", "Pident", "Taxid")
