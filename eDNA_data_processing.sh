06b0ad9fec60d115932c6fe9f1e647da149b3a17
smartise <olivier1996collard@hotmail.fr>
2024-04-29 10:08:11 +0200

    changing the percentage identity

--- eDNA_data_processing.sh
+++ eDNA_data_processing.sh
@@ -7,17 +7,13 @@
 cutadapt -g TTTCTGTTGGTGCTGATATTGCCHACWAAYCATAAAGATATYGG --revcomp -o Without_Primers.fasta Sequences.fasta
 cutadapt -g ACTTGCCTGTCGCTCTATCTTCAWACTTCVGGRTGVCCAAARAATCA --revcomp -o Without_Primers2.fasta Without_Primers.fasta
 
-# Doing the blast over the EBE database  (it can take a while)
+# Doing the blast over the EBE database with match with at least 99% query  (it can take a while)
 
-blastn -query Without_Primers2.fasta -db /mnt/ebe/blobtools/nt/nt -out Blast_res.txt -outfmt "6 qseqid qlen sseqid slen length gaps qcovs pident staxids"
-
-# Cleaning the low query value allignement 
-
-Rscript Remove_lowquery.R
+blastn -query Without_Primers2.fasta -db /mnt/ebe/blobtools/nt/nt -out Blast_res.txt -outfmt "6 qseqid qlen sseqid slen length gaps qcovs pident staxids" -perc_identity 99
 
 # recovering the identity of each tax ID and putting it into a tsv file (this can also take a while)
 
-datasets summary taxonomy taxon $(<High_query_blast_res.txt) --as-json-lines | dataformat tsv taxonomy --template tax-summary > output.tsv
+datasets summary taxonomy taxon $(<Blast_res.txt) --as-json-lines | dataformat tsv taxonomy --template tax-summary > output.tsv
 
 
 
