76962acb4de0ae1e6c3ed1050e3d78d61995cbb7
smartise <olivier1996collard@hotmail.fr>
2024-04-29 11:01:02 +0200

    changing for remote

--- eDNA_data_processing.sh
+++ eDNA_data_processing.sh
@@ -2,19 +2,22 @@
 ###  primer removing using of COI using Cutadapt  ###
 ########################
 
-# removing the primers 
+echo "###################removing the primers######################@"
 
 cutadapt -g TTTCTGTTGGTGCTGATATTGCCHACWAAYCATAAAGATATYGG --revcomp -o Without_Primers.fasta Sequences.fasta
 cutadapt -g ACTTGCCTGTCGCTCTATCTTCAWACTTCVGGRTGVCCAAARAATCA --revcomp -o Without_Primers2.fasta Without_Primers.fasta
 
-# Doing the blast over the EBE database with match with at least 99% query  (it can take a while)
+# Doing the blast over NCBI data base (it can take a while)
 
-blastn -query Without_Primers2.fasta -db /mnt/ebe/blobtools/nt/nt -out Blast_res.txt -outfmt "6 qseqid qlen sseqid slen length gaps qcovs pident staxids" -perc_identity 99
+echo "##################Doing the blast over NCBI data base (it can take a while)####################"
+
+blastn -query Without_Primers2.fasta -db nt -remote -out Blast_res.txt -outfmt "6 qseqid qlen sseqid slen length gaps qcovs pident staxids" -perc_identity 99
 
 # recovering the identity of each tax ID and putting it into a tsv file (this can also take a while)
 
-datasets summary taxonomy taxon $(<Blast_res.txt) --as-json-lines | dataformat tsv taxonomy --template tax-summary > output.tsv
+echo "########recovering the identity of each tax ID and putting it into a tsv file (this can also take a while)###########"
 
+datasets summary taxonomy taxon $(<High_query_blast_res.txt) --as-json-lines | dataformat tsv taxonomy --template tax-summary > output.tsv
 
 
 
